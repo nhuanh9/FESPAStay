d4e1647d875179d64e9acf45c07580635942de10
NA <chopperlouis@gmail.com>
2020-07-25 22:00:21 +0700

    thêm model, service houseImage

--- src/app/Components/Blocks/carousel/carousel.component.ts
+++ src/app/Components/Blocks/carousel/carousel.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HouseImages} from "../../../model/houseImages";
+import {Subscription} from "rxjs";
+import {HouseImagesService} from "../../../Services/house-images.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-carousel',
@@ -6,10 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carousel.component.scss']
 })
 export class CarouselComponent implements OnInit {
+  houseImages: HouseImages[];
+  sub: Subscription;
 
-  constructor() { }
+  constructor(private houseImagesService: HouseImagesService,
+              private activatedRoute: ActivatedRoute) {
+  }
 
   ngOnInit() {
+    this.houseImagesService.getImagesByIdHouse('4').subscribe(result => {
+      this.houseImages = result;
+      console.log(this.houseImages);
+    })
   }
 
 }
---
+++ src/app/Services/house-images.service.ts
@@ -0,0 +1,32 @@
+import {Injectable} from '@angular/core';
+import {environment} from "../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {HouseImages} from "../model/houseImages";
+import {Room} from "../model/room";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HouseImagesService {
+  API_URL = environment.apiUrl + '/images'
+
+  constructor(private http: HttpClient) {
+  }
+
+  create(image): Observable<HouseImages> {
+    return this.http.post<HouseImages>(this.API_URL, image);
+  }
+
+  delete(id: string): Observable<HouseImages> {
+    return this.http.delete<HouseImages>(this.API_URL + `/${id}`);
+  }
+
+  getImagesByIdHouse(id: string): Observable<HouseImages[]> {
+    return this.http.get<HouseImages[]>(this.API_URL + `/search-house/${id}`)
+  }
+
+  getList(): Observable<HouseImages[]> {
+    return this.http.get<HouseImages[]>(this.API_URL)
+  }
+}
